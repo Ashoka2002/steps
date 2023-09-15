a1a85882b068b810eddafc31dbf91a883069fa57
Ashok Choudhary <ashoka8769@gmail.com>
2023-09-15 09:46:15 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 
 function App() {
+  const bgColor = { backgroundColor: "#7950f2", color: "#fff" };
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className="active">1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button onClick={() => setStep(step - 1)} disabled={step <= 1 ? true : false} style={bgColor}>
+              Previous
+            </button>
+            <button onClick={() => setStep(step + 1)} disabled={step >= 3 ? true : false} style={bgColor}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
